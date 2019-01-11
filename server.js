14b75f9235c9b46cb9c16d94be9ff07cde0d4847
raul-f <raulf.dev@gmail.com>
2019-01-11 18:53:33 -0300

    Added /api/whoami general handler

--- server.js
+++ server.js
@@ -24,9 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+// app body
+app.use('/api/whoami', (req, res) => {
+	console.log(req.headers);
+	req.ipaddress = ((req.headers['x-forwarded-for']).split(','))[0];
+	res.json({ipaddress: req.ipaddress, language: req.headers['accept-language'], software: req.headers['user-agent']});
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
